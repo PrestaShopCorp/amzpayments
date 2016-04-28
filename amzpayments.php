a6bdbfd4f3317f86df6a7c4c6921e8a04217d5d7
paeddl <patrick.weinstein@patworx.de>
2016-04-28 09:56:25 +0200

    [+] MO : amzpayments - Update to v2.0.24

--- amzpayments.php
+++ amzpayments.php
@@ -148,7 +148,7 @@ class AmzPayments extends PaymentModule
     {
         $this->name = 'amzpayments';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.23';
+        $this->version = '2.0.24';
         $this->author = 'patworx multimedia GmbH';
         $this->need_instance = 1;
         

7beca953289f5bfeba9dc310ca49d4f52ceec095
paeddl <patrick.weinstein@patworx.de>
2017-02-17 07:57:00 +0100

    v2.0.57 - rebranding Amazon Pay

--- amzpayments.php
+++ amzpayments.php
@@ -152,7 +152,7 @@ class AmzPayments extends PaymentModule
     {
         $this->name = 'amzpayments';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.55';
+        $this->version = '2.0.57';
         $this->author = 'patworx multimedia GmbH';
         $this->need_instance = 1;
         
